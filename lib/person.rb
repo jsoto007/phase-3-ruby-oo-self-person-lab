f6659e174c3eb283b1768d4497d0ea9e1505e4b5
Jose Soto <jsoto7087@gmail.com>
2023-01-21 02:07:01 -0500

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -26,11 +26,11 @@ class Person
     end 
 
     def happy?
-        @happiness > 7 ? true : false
+        self.happiness > 7 ? true : false
     end 
 
     def clean?
-        @hygiene > 7 ? true : false
+        self.hygiene > 7 ? true : false
     end 
 
     def hygiene=(new_hygiene)
@@ -41,6 +41,43 @@ class Person
         else 
             @hygiene = new_hygiene
         end 
+
+        def get_paid(amount)
+            self.bank_account += amount
+            return "all about the benjamins"
+        end 
+
+        def take_bath
+            self.hygiene += 4
+            return "♪ Rub-a-dub just relaxing in the tub ♫"
+        end 
+
+        def work_out
+            self.hygiene -= 3
+            self.happiness += 2
+            return '♪ another one bites the dust ♫'
+        end 
+
+        def call_friend(friend)
+            self.happiness += 3
+            friend.happiness += 3
+            return "Hi #{friend.name}! It's #{self.name}. How are you?"
+        end
+
+        def start_conversation(person, topic)
+            if topic == "politics"
+                self.happiness -= 2
+                person.happiness  -= 2
+                return "blah blah partisan blah lobbyist"
+            elsif topic == "weather"
+                self.happiness += 1
+                person.happiness += 1
+                return 'blah blah sun blah rain'
+            else
+                return 'blah blah blah blah blah'
+            end
+        end 
+
     end 
 
 end
